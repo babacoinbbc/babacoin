83f2d187a27f5c0b6302f237ff3cf6e86766b0a6
Babacoin Network <113639962+babacoinbbc@users.noreply.github.com>
2026-04-17 02:04:30 +0300

    depends: update gmp 6.1.2 → 6.3.0, use GNU FTP mirror
    gmplib.org no longer hosts .sha256 hash files for 6.1.2,
    causing 404 during depends build. Switch to GNU FTP which
    has stable URLs, and update to latest GMP 6.3.0.

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,8 +1,8 @@
 package=gmp
-$(package)_version=6.1.2
-$(package)_download_path=https://gmplib.org/download/gmp
+$(package)_version=6.3.0
+$(package)_download_path=https://ftp.gnu.org/gnu/gmp
 $(package)_file_name=gmp-$($(package)_version).tar.bz2
-$(package)_sha256_hash=5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2
+$(package)_sha256_hash=ac28211a7cfb609bae2e2c8d6058d66c8fe96434f740cf6284b4404bfd2eb4aa
 
 define $(package)_set_vars
 $(package)_config_opts+=--enable-cxx --enable-fat --with-pic --disable-shared
@@ -20,4 +20,3 @@ endef
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
-
