32ee43e66874ce0acd90f45cbd6e38a8d38c2609
Babacoin Network <113639962+babacoinbbc@users.noreply.github.com>
2026-04-17 14:30:06 +0300

    fix: use boost::placeholders in transactiontablemodel.cpp

--- src/qt/transactiontablemodel.cpp
+++ src/qt/transactiontablemodel.cpp
@@ -3,6 +3,10 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <qt/transactiontablemodel.h>
+// Boost 1.73+: _1, _2 placeholders moved to boost::placeholders namespace
+#include <boost/bind/bind.hpp>
+using namespace boost::placeholders;
+
 
 #include <qt/guiutil.h>
 #include <qt/optionsmodel.h>
